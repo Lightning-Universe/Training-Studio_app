e4f123ba339e400abd9b37dd69db9c9190f276c4
thomas chaton <thomas@grid.ai>
2022-09-01 19:20:49 +0200

    update

--- tests/commands/sweep/test_show_sweep.py
+++ tests/commands/sweep/test_show_sweep.py
@@ -5,10 +5,13 @@ from unittest.mock import MagicMock
 
 import pytest
 import requests
+from lightning.app.storage import Drive
 from rich.table import Table
 
 from lightning_hpo.commands.sweep import show_sweeps
 from lightning_hpo.commands.sweep.show_sweeps import ShowSweepsCommand
+from lightning_hpo.components.sweep import Sweep, SweepConfig
+from lightning_hpo.controllers.sweeper import SweepController
 
 
 def test_show_sweeps_client(monkeypatch):
@@ -55,3 +58,27 @@ def test_show_sweeps_client(monkeypatch):
     assert table.columns[3]._cells[0] == "{'model.lr': 0.04490742315966646}"
 
     sys.argv = ori_sys_argv
+
+
+def test_show_sweeps_server(monkeypatch):
+
+    with open(os.path.join(os.path.dirname(__file__), "sweep_1.json"), "rb") as f:
+        data = json.load(f)
+
+    sweep_config = SweepConfig(**data[0])
+    sweep_config.logger = "streamlit"
+    Sweep.from_config(config=sweep_config)
+
+    sweep_controller = SweepController(Drive("lit://code"))
+    sweep_controller.db_url = ""
+    resp = MagicMock()
+    resp.json.return_value = data
+    resp.status_code = 200
+    get = MagicMock(return_value=resp)
+    monkeypatch.setattr(requests, "get", get)
+    result = sweep_controller.show_sweeps()
+    assert result == data
+    expected = (
+        '{"cls_name": "SweepConfig", "cls_module": "lightning_hpo.commands.sweep.run_sweep", "data": "", "id": null}'
+    )
+    assert get._mock_call_args.kwargs["data"] == expected
