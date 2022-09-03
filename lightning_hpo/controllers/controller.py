9d77277c656ae729290c66809ed6cb1285fac296
thomas chaton <thomas@grid.ai>
2022-09-03 20:52:21 +0200

    update

--- lightning_hpo/controllers/controller.py
+++ lightning_hpo/controllers/controller.py
@@ -6,7 +6,7 @@ from lightning.app.storage import Drive
 from lightning.app.structures import Dict
 from sqlmodel import SQLModel
 
-from lightning_hpo.components.servers.db.database import Database
+from lightning_hpo.components.servers.db import Database
 
 
 class Controller(LightningFlow):
