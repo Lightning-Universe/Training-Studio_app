ee3725141af5a8d9a831757dfafdbe9186b94162
Ethan Harris <ethanwharris@gmail.com>
2022-09-09 11:04:45 +0100

    Basic handling of request errors (#98)

--- lightning_hpo/app/ui/src/hooks/useClientDataState.tsx
+++ lightning_hpo/app/ui/src/hooks/useClientDataState.tsx
@@ -1,3 +1,4 @@
+import { useSnackbar } from 'lightning-ui/src/design-system/components';
 import React, { useEffect } from 'react';
 import { AppClient } from '../generated';
 
@@ -22,12 +23,25 @@ const clientDataContexts = {
 
 export const ClientDataProvider = (props: { endpoint: keyof typeof clientEndpoints; children: React.ReactNode }) => {
   const [state, dispatch] = React.useReducer((state: any[], newValue: any[]) => newValue, []);
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
-    clientEndpoints[props.endpoint](appClient).then(data => dispatch(data));
+    const post = () => {
+      clientEndpoints[props.endpoint](appClient)
+        .then(data => dispatch(data))
+        .catch(error => {
+          enqueueSnackbar({
+            title: 'Error Fetching Data',
+            children: 'Try reloading the page',
+            severity: 'error',
+          });
+        });
+    };
+
+    post();
 
     const interval = setInterval(() => {
-      clientEndpoints[props.endpoint](appClient).then(data => dispatch(data));
+      post();
     }, 1000);
 
     return () => clearInterval(interval);
