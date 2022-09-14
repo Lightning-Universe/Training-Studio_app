6da599e1b309b76e3f05c0c7586c524419d57e9a
Ethan Harris <ethanwharris@gmail.com>
2022-09-14 20:36:08 +0100

    More tests

--- tests/features/notebook.feature
+++ tests/features/notebook.feature
@@ -1,4 +1,4 @@
-Feature: Notebook
+Feature: Start Notebook
   As an app user,
   I want to start a jupyter interactive session on a instance type of my choice,
   so that I can develop and run my model, or explore my data or artifacts.
@@ -7,4 +7,19 @@ Feature: Notebook
     Given I am connected to the app
      When I execute lightning run notebook
      Then I can see a notebook in the UI
+      And the notebook status in the UI is running
       And I can open the notebook
+
+  Scenario: Stop notebook CLI
+    Given I am connected to the app
+      And a notebook is running
+     When I execute lightning stop notebook
+     Then I can see a notebook in the UI
+      And the notebook status in the UI is stopped
+
+  Scenario: View notebooks CLI
+    Given I am connected to the app
+      And a notebook is running
+     When I execute lightning show notebooks
+     Then I can see a notebook in the terminal
+      And the notebook status in the terminal is running
--- tests/features/steps/notebook.py
+++ tests/features/steps/notebook.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 import uuid
 
-from behave import then, when
+from behave import given, then, when
 from playwright.sync_api import expect, Page
 
 
@@ -18,6 +18,48 @@ def step_impl(context):
         env=os.environ.copy(),
     )
     process.wait()
+
+
+@when("I execute lightning stop notebook")
+def step_impl(context):
+    cmd = [sys.executable, "-m", "lightning", "stop", "notebook", f"--name={context.notebook_name}"]
+
+    process = subprocess.Popen(
+        cmd,
+        env=os.environ.copy(),
+    )
+    process.wait()
+
+
+@when("I execute lightning show notebooks")
+def step_impl(context):
+    cmd = [sys.executable, "-m", "lightning", "show", "notebooks"]
+
+    process = subprocess.Popen(
+        cmd,
+        env=os.environ.copy(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    process.wait()
+
+    context.stdout = process.stdout.read().decode("UTF-8")
+    context.stderr = process.stderr.read().decode("UTF-8")
+
+
+@given("a notebook is running")
+def step_impl(context):
+    context.execute_steps(
+        """
+        When I execute lightning run notebook
+    """
+    )
+
+    page: Page = context.page
+
+    locator = page.frame_locator("iframe").locator("table tbody tr", has_text=context.notebook_name)
+    locator = locator.locator("text=Running")
+    locator.wait_for(timeout=60 * 1000)
 
 
 @then("I can see a notebook in the UI")
@@ -36,3 +78,48 @@ def step_impl(context):
     notebook_url = locator.locator("a", has_text="Open").get_attribute("href")
     page.goto(notebook_url)
     expect(page.locator("title")).to_have_text("JupyterLab")
+
+
+def check_notebook_status_ui(context, status):
+    page: Page = context.page
+
+    locator = page.frame_locator("iframe").locator("table tbody tr", has_text=context.notebook_name)
+    locator = locator.locator(f"text={status}")
+    expect(locator).to_be_visible(timeout=60 * 1000)
+
+
+@then("the notebook status in the UI is stopped")
+def step_impl(context):
+    check_notebook_status_ui(context, "Stopped")
+
+
+@then("the notebook status in the UI is running")
+def step_impl(context):
+    check_notebook_status_ui(context, "Running")
+
+
+@then("I can see a notebook in the terminal")
+def step_impl(context):
+    # Notebook name can be clipped so just look for first 8 characters
+    assert context.notebook_name[:8] in context.stdout
+
+
+def check_notebook_status_terminal(context, status):
+    header_row = next(filter(lambda line: "name" in line, context.stdout.split("\n")))
+    keys = [key.strip() for key in header_row.split("┃")[1:-1]]
+
+    notebook_row = next(filter(lambda line: context.notebook_name[:8] in line, context.stdout.split("\n")))
+    values = [value.strip() for value in notebook_row.split("│")[1:-1]]
+
+    notebook_details = {key: value for key, value in zip(keys, values)}
+    assert notebook_details["stage"] == status
+
+
+@then("the notebook status in the terminal is stopped")
+def step_impl(context):
+    check_notebook_status_terminal(context, "stopped")
+
+
+@then("the notebook status in the terminal is running")
+def step_impl(context):
+    check_notebook_status_terminal(context, "running")
