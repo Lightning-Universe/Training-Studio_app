3b16674bc51b153f0ed4fa118995c73b567d0769
thomas chaton <thomas@grid.ai>
2022-10-05 19:21:52 +0200

    update

---
+++ lightning_hpo/commands/drive/show.py
@@ -0,0 +1,29 @@
+from lightning.app.utilities.commands import ClientCommand
+from rich.console import Console
+from rich.table import Table
+
+
+class ShowDriveCommand(ClientCommand):
+
+    DESCRIPTION = "Command to show the drives"
+
+    def run(self) -> None:
+        response = self.invoke_handler()
+
+        table = Table(
+            "name",
+            "source",
+            "mount_path",
+            title="Drives",
+            show_header=True,
+            header_style="bold green",
+        )
+
+        for drive in response:
+            table.add_row(
+                drive["name"],
+                drive["source"],
+                drive["mount_path"],
+            )
+        console = Console()
+        console.print(table)
