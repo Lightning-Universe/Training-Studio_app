0cf9de9148e746768e1317f66fb83e675afcd015
Ethan Harris <ethanwharris@gmail.com>
2022-10-21 20:42:06 +0100

    Fix

--- tests/commands/experiment/test_run.py
+++ tests/commands/experiment/test_run.py
@@ -59,7 +59,7 @@ def test_experiment_run_parsing_no_arguments(monkeypatch):
             stage="not_started",
             desired_stage="running",
             disk_size=80,
-            data=[],
+            data={},
         )
         expected.sweep_id = config.sweep_id
         expected.username = config.username
@@ -124,7 +124,7 @@ def test_experiment_run_parsing_arguments(monkeypatch):
             stage="not_started",
             desired_stage="running",
             disk_size=80,
-            data=["example"],
+            data={"example", None},
         )
         expected.sweep_id = config.sweep_id
         expected.username = config.username
@@ -199,7 +199,7 @@ def test_experiment_run_parsing_requirements(monkeypatch):
             stage="not_started",
             desired_stage="running",
             disk_size=80,
-            data=[],
+            data={},
         )
         expected.sweep_id = config.sweep_id
         expected.username = config.username
