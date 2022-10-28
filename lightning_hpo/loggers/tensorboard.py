c1b550df8546e927c2bae2168e65ce423136fdb0
Luca Antiga <luca.antiga@gmail.com>
2022-10-28 15:26:37 +0200

    Fix imports (#206)
    Co-authored-by: Luca Antiga <luca@lightning.ai>
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- lightning_hpo/loggers/tensorboard.py
+++ lightning_hpo/loggers/tensorboard.py
@@ -4,6 +4,13 @@ from pathlib import Path
 from time import time
 from typing import Any, Dict, Optional
 
+from lightning_hpo.utilities.imports import _IS_PYTORCH_LIGHTNING_AVAILABLE
+
+if _IS_PYTORCH_LIGHTNING_AVAILABLE:
+    import pytorch_lightning
+else:
+    import lightning.pytorch as pytorch_lightning
+
 from fsspec.implementations.local import LocalFileSystem
 from lightning import LightningFlow
 from lightning.app.storage import Drive
@@ -84,8 +91,6 @@ class TensorboardLogger(Logger):
         return []
 
     def configure_tracer(self, tracer, sweep_id: str, experiment_id: int, experiment_name: str, params: Dict[str, Any]):
-        import pytorch_lightning
-
         # Create a space logs under the sweep_id folder
         drive = Drive(f"lit://{sweep_id}", component_name=experiment_name, allow_duplicates=True)
         use_localhost = "LIGHTNING_APP_STATE_URL" not in os.environ
