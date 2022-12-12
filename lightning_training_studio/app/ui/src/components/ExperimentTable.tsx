74f3b6545857225715dcca130bd65c09d5aae845
thomas <thomas@thomass-MacBook-Pro.local>
2022-12-12 19:19:45 +0000

    update

--- lightning_training_studio/app/ui/src/components/ExperimentTable.tsx
+++ lightning_training_studio/app/ui/src/components/ExperimentTable.tsx
@@ -206,8 +206,7 @@ export function Experiments() {
         </UserGuideBody>
         <UserGuideComment>Run a sweep</UserGuideComment>
         <UserGuideBody>
-          lightning run sweep train.py --model.lr "[0.001, 0.01, 0.1]" --data.batch "[32, 64]"
-          --algorithm="grid_search"
+          lightning run sweep train.py --model.lr "[0.001, 0.01]" --data.batch "[32, 64]" --algorithm="grid_search"
         </UserGuideBody>
       </UserGuide>
     );
