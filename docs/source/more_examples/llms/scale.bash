24f9e760ffe1ffe0bd7b9579e2a8409114ab1a45
thomas <thomas@thomass-MacBook-Pro.local>
2022-12-12 15:03:04 +0000

    update

---
+++ docs/source/more_examples/llms/scale.bash
@@ -0,0 +1,14 @@
+# Run 1.4 Billion parameters model on 4 GPUS
+lightning run experiment train.py \
+    --name="lightning-gpt2-xl" \
+    --model_type="gpt2-xl" \
+    --batch_size=64  \
+    --cloud_compute="gpu-fast-multi"
+
+# Run 2.9 Billion parameters model on 4 GPUS
+lightning run experiment train.py \
+    --name="lightning-gpt2-xxl" \
+    --model_type="gpt2-xxl" \
+    --batch_size=16  \
+    --cloud_compute="gpu-fast-multi" \
+    --strategy="deepspeed_stage_3_offload"
