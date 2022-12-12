e57c45e992eb9fde60055a824ce91e7491f1aee5
thomas <thomas@thomass-MacBook-Pro.local>
2022-12-12 16:32:20 +0000

    add deps

--- docs/source/more_examples/llms/scale.bash
+++ docs/source/more_examples/llms/scale.bash
@@ -9,6 +9,7 @@ lightning run experiment train.py \
 lightning run experiment train.py \
     --name="lightning-gpt2-xxl" \
     --model_type="gpt2-xxl" \
+    --requirements="requirements-deepspeed.txt" \
     --batch_size=16  \
     --cloud_compute="gpu-fast-multi" \
     --strategy="deepspeed_stage_3_offload"
